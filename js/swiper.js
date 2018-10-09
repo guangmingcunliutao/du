97a1f0b1ff4d3e5c939234a66bd24d9030afdb3d
xiuse_liutao <1018694341@qq.com>
2018-10-09 10:15:33 +0800

    登录注册页面

---
+++ js/swiper.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    autoplay:true,
+    loop:true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+  });
