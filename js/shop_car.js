bde1a067b5fb2c873421d88ede8e3fc5664a79fd
xiuse_liutao <1018694341@qq.com>
2018-10-16 17:23:08 +0800

    购物车页面数据渲染，待完善

--- js/shop_car.js
+++ js/shop_car.js
@@ -1,21 +1,82 @@
-var shop_car = (function(){
+var shop_car = (function () {
     return {
-        init:function(){
+        init: function () {
             this.event();
+            this.pro_item_wrap = $('.pro_item_wrap');
+            this.getCarList();
         },
-        event:function(){
+        event: function () {
             var _this = this;
+            $.get('http://localhost:1101/php/du/php/product.php', _this.allShopData);
         },
+        getCarList: function () {
+            // 获取localstorage里面的商品列表
+            shop_car.carListData = localStorage.shop_list || '[]';
+            // 如果为空，显示空购物车
+            if (shop_car.carListData == '[]') {
+                $('.car_list_wrap').remove();
+            } else {
+                $('.no_shop_wrap').remove();
+            }
+            // 将获取到的json字符串转换为对象
+            shop_car.carListData = JSON.parse(shop_car.carListData);
+        },
+        // 获取数据库里面所有产品的数据
+        allShopData: function (data) {
+            data = JSON.parse(data);
+            var arr = [];
+            for (var i = 0; i < shop_car.carListData.length; i++) {
+                var shop_data;
+                var price;
+                for (var j = 0; j < data.length; j++) {
+                    // 只使用localstorage里面保存的id相同的数据
+                    if (shop_car.carListData[i].pro_id == data[j].id) {
+                        if(data[j].pro_sale == '' || data[j].pro_sale == null){
+                            data[j].pro_sale = '';
+                            price = data[j].pro_cost;
+                        }else{
+                            price = data[j].pro_sale;
+                        }
+                        shop_data = data[j];
+                    }
+                }
+                arr.push(`<div class="pro_item">
+                            <div class="checkWarp">
+                                <span class="check"></span>
+                            </div>
+                        <div class="item_content">
+                            <div class="pro_img">
+                                <img src="${shop_data.pro_img}" alt="">
+                            </div>
+                            <div class="name_desc_wrap">
+                                <div class="pro_name">
+                                    ${shop_data.pro_name}
+                                    ${shop_data.pro_desc}
+                                </div>
+                            </div>
+                            <div class="pro_price">
+                                <div class="pro_sale">${shop_data.pro_sale}</div>
+                                <div class="pro_cost">￥${shop_data.pro_cost}</div>
+                            </div>
+                            <div class="count_wrap">
+                                <div class="inp_num">
+                                    <div class="num_sub"></div>
+                                    <div class="sub_tip">不能再少了</div>
+                                    <input type="number" max="10" min="1" value="${shop_car.carListData[i].count}" class="countInp">
+                                    <div class="num_add"></div>
+                                    <div class="add_tip">不能再多了</div>
+                                </div>
+                            </div>
+                            <div class="sum">
+                                ￥${price*shop_car.carListData[i].count}
+                            </div>
+                            <div class="action">
+                                <span>删除</span>
+                            </div>
+                        </div>
+                    </div>`);
+            }
+            shop_car.pro_item_wrap.append(arr);
+        }
     }
 }());
-/* // 获取localstorage里面的商品列表
-this.carListData = localStorage.shop_list || '[]';
-// 如果为空，显示空购物车
-if(this.carListData == '[]'){
-    $('.car_list_wrap').remove();
-}else{
-    $('.no_shop_wrap').remove();
-}
-// 将获取到的json字符串转换为对象
-this.carListData = JSON.parse(this.carListData);
-console.log(this.carListData); */
--- php/product.php
+++ php/product.php
@@ -7,11 +7,13 @@ $link = new mysqli('localhost','root','9511','du_db','3306');
 // 设置数据库字符集
 $link->query("set names 'utf8'");
 $link->query("set charactor set 'utf8'");
-$select = "SELECT * from product";
-$result = $link -> query($select);
+$select_sql = "SELECT * from product";
+$result = $link -> query($select_sql);
+// 返回数据库结果集行数
 $num = mysqli_num_rows($result);
 $arr = array();
 for($i=0;$i<$num;$i++){
+    // 利用fetch_assoc方法查找数据只返回一条，并且往后查找的特行，将数据push到数组
     $rows = $result -> fetch_assoc();
     array_push($arr,$rows);
 }
